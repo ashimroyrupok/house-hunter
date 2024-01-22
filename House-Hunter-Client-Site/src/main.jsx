5acff86fa5325e109a653dfe321f78a831b1f8ed
ashimroyrupok <ashimroyrupok@gmail.com>
2024-01-22 23:18:24 +0600

    added login page

--- House-Hunter-Client-Site/src/main.jsx
+++ House-Hunter-Client-Site/src/main.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import { RouterProvider } from 'react-router-dom';
+import router from './route/router.jsx';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import {
+
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <QueryClientProvider client={queryClient}>
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
+  </QueryClientProvider>
+);
---
+++ House-Hunter-Client-Site/src/route/router.jsx
@@ -0,0 +1,24 @@
+import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "../Layout/MainLayout/MainLayout";
+import Home from "../pages/Home/Home";
+import Login from "../pages/Login/Login";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout></MainLayout>,
+    children:[
+        {
+            path:'/',
+            element: <Home></Home>
+        },
+        {
+            path:"/login",
+            element: <Login></Login>
+        },
+        
+    ]
+  },
+]);
+
+export default router;
